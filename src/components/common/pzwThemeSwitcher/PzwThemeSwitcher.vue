d08605171f24a9d2964f9b1adffcb4a2f5314f85
Paweł Bińkowski <pawel.binkowski@over-cloud.pl>
2025-01-30 18:08:47 +0100

    feat: add theme switcher component

---
+++ src/components/common/pzwThemeSwitcher/PzwThemeSwitcher.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useTheme } from 'vuetify';
+
+const theme = useTheme();
+const isDark = ref(theme.global.current.value.dark);
+
+function toggleTheme() {
+  isDark.value = !isDark.value;
+  theme.global.name.value = isDark.value ? 'dark' : 'light';
+}
+</script>
+
+<template>
+  <v-switch
+    v-model="isDark"
+    inset
+    @click="toggleTheme"
+    true-icon="mdi-moon-waning-crescent"
+    false-icon="mdi-white-balance-sunny"
+    hide-details
+  >
+  </v-switch>
+</template>
