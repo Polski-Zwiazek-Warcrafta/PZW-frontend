6e45d091175d7b1d1c4b1454465ac6341e7161f5
Paweł Bińkowski <pawel.binkowski@over-cloud.pl>
2025-01-30 18:09:14 +0100

    feat: add top bar component and connect with main page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { t } from '@/plugins/i18n';
+import TopBar from './components/topBar/TopBar.vue';
 </script>
 <template>
-  <v-container>
-    <v-row>
-      <v-col>
-        <h1>{{ t('mainPage.text') }}</h1>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-app>
+    <TopBar />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
---
+++ src/components/common/pzwDropdownMenu.vue/PzwDropdownMenu.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { defineProps, defineSlots } from 'vue';
+import PzwButton from '../pzwButton/PzwButton.vue';
+
+defineProps<{
+  activatorText?: string;
+}>();
+
+defineSlots<{
+  content: void;
+  activatorContent: void;
+}>();
+</script>
+
+<template>
+  <v-menu>
+    <template #activator="{ props, isActive }">
+      <PzwButton
+        v-bind="{ ...$attrs, ...props }"
+        variant="text"
+        :append-icon="`mdi-chevron-${isActive ? 'up' : 'down'}`"
+      >
+        <slot name="activatorContent">
+          {{ activatorText }}
+        </slot>
+      </PzwButton>
+    </template>
+
+    <slot name="content">
+      <v-list></v-list>
+    </slot>
+  </v-menu>
+</template>
