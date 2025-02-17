632fe9cbc3cbc81665540f11f2b5624d83929bd7
Paweł Bińkowski <pawel.binkowski@over-cloud.pl>
2025-02-17 17:55:39 +0100

    feat: add logo banner

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import { t } from '@/plugins/i18n';
 import TopBar from './components/topBar/TopBar.vue';
 import NavigationBar from './components/navigationBar/NavigationBar.vue';
+import LogoBanner from './components/logoBanner/LogoBanner.vue';
 </script>
 <template>
   <v-app>
     <NavigationBar />
     <TopBar />
     <v-main>
+      <LogoBanner />
       <router-view />
     </v-main>
   </v-app>
---
+++ src/components/logoBanner/LogoBanner.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import logo from '@/assets/logo.png';
+</script>
+
+<template>
+  <div
+    class="w-full d-flex align-center flex-column justify-center text-center"
+  >
+    <h1 class="mb-2">Polski Związek Warcrafta</h1>
+    <v-img :src="logo" width="100" height="100" />
+  </div>
+</template>
+<style lang="scss" scoped>
+h1 {
+  font-size: 2.5rem;
+  font-weight: 700;
+  color: var(--primary);
+}
+</style>
